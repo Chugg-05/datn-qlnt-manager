0518ec2527ee6fa97945ab10a38a695dbccb80c4
Nguyen Chung <chungnvph52424@gmail.com>
2025-06-17 00:23:11 +0700

    feat(user): add filter user

--- src/main/java/com/example/datn_qlnt_manager/dto/filter/UserFilter.java
+++ src/main/java/com/example/datn_qlnt_manager/dto/filter/UserFilter.java
@@ -1,6 +1,7 @@
 package com.example.datn_qlnt_manager.dto.filter;
 
 import com.example.datn_qlnt_manager.common.Gender;
+import com.example.datn_qlnt_manager.common.UserStatus;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -10,9 +11,10 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserFilter {
-    String name;
+    String fullName;
     String email;
-    String phone;
-    String address;
+    String phoneNumber;
     Gender gender;
+    UserStatus userStatus;
+    String role;
 }
