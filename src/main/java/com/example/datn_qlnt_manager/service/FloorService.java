fcd45f8f8f33cbf89a0e67e7d4a6f9fe9936a4c4
Khanhpisaa <khanhhnph52275@gmail.com>
2025-06-19 16:42:40 +0700

    fix(floor): fix api soft delete floor

--- src/main/java/com/example/datn_qlnt_manager/service/FloorService.java
+++ src/main/java/com/example/datn_qlnt_manager/service/FloorService.java
@@ -1,6 +1,5 @@
 package com.example.datn_qlnt_manager.service;
 
-import com.example.datn_qlnt_manager.dto.ApiResponse;
 import com.example.datn_qlnt_manager.dto.PaginatedResponse;
 import com.example.datn_qlnt_manager.dto.filter.FloorFilter;
 
@@ -13,17 +12,18 @@ import com.example.datn_qlnt_manager.dto.response.floor.FloorResponse;
 public interface FloorService {
 
     //thêm
-    ApiResponse<FloorResponse> createFloor(FloorCreationRequest request);
+    FloorResponse createFloor(FloorCreationRequest request);
 
     // hiển thị, lọc, tìm kiếm
     PaginatedResponse<FloorResponse> filterFloors(FloorFilter filter, int page, int size);
 
+    //sửa
+    FloorResponse updateFloor(String id, FloorUpdateRequest request);
+
     // xóa mềm (trạng thái tầng về KHONG_SU_DUNG)
     void softDeleteFloorById(String floorId);
 
-    //sửa
-    ApiResponse<FloorResponse> updateFloor(String id, FloorUpdateRequest request);
 
     // xóa luôn
-    ApiResponse<Void> deleteFloor(String floorId);
+    void deleteFloor(String floorId);
 }
