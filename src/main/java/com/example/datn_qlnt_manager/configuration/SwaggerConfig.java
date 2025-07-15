85099c0e2cdc6a9d48255814b8a530e92d3894b2
Nguyen Chung <chungnvph52424@gmail.com>
2025-07-15 17:31:47 +0700

    háchchore(swagger): update configuration

--- src/main/java/com/example/datn_qlnt_manager/configuration/SwaggerConfig.java
+++ src/main/java/com/example/datn_qlnt_manager/configuration/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.example.datn_qlnt_manager.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +19,21 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Quản lý nhà trọ API - TroHub")
                         .version("1.0.0")
-                        .description("API - Website quản lý nhà trọ"));
+                        .description("API - Website quản lý nhà trọ"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")); // Áp dụng toàn bộ API
     }
 
     // Tạo nhóm OpenAPI cho các public endpoint
     @Bean
     public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder().group("trohub-api").pathsToMatch("/**").build();
+        return GroupedOpenApi.builder()
+                .group("trohub-api")
+                .pathsToMatch("/**")
+                .build();
     }
 }
